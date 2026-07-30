38f9cc0f90d0b76b1934029fd59a09598b63f006
Luca Caputo <luca.caputo@welld.ch>
2026-07-30 14:09:00 +0200

    chore: convert svgs to pngs

--- constants.ts
+++ constants.ts
@@ -1,29 +1,29 @@
 import { Asset } from "expo-asset";
 import { ImageSourcePropType } from "react-native";
 
-import card01 from "./assets/images/cards/card_01.svg";
-import card02 from "./assets/images/cards/card_02.svg";
-import card03 from "./assets/images/cards/card_03.svg";
-import card04 from "./assets/images/cards/card_04.svg";
-import card05 from "./assets/images/cards/card_05.svg";
-import card06 from "./assets/images/cards/card_06.svg";
-import card07 from "./assets/images/cards/card_07.svg";
-import card08 from "./assets/images/cards/card_08.svg";
-import card09 from "./assets/images/cards/card_09.svg";
-import card10 from "./assets/images/cards/card_10.svg";
-import card11 from "./assets/images/cards/card_11.svg";
-import card12 from "./assets/images/cards/card_12.svg";
-import card13 from "./assets/images/cards/card_13.svg";
-import card14 from "./assets/images/cards/card_14.svg";
-import card15 from "./assets/images/cards/card_15.svg";
-import card16 from "./assets/images/cards/card_16.svg";
-import card17 from "./assets/images/cards/card_17.svg";
-import card18 from "./assets/images/cards/card_18.svg";
-import card19 from "./assets/images/cards/card_19.svg";
-import card20 from "./assets/images/cards/card_20.svg";
-import card21 from "./assets/images/cards/card_21.svg";
-import card22 from "./assets/images/cards/card_22.svg";
-import card23 from "./assets/images/cards/card_23.svg";
+import card01 from "./assets/images/cards/card_01.png";
+import card02 from "./assets/images/cards/card_02.png";
+import card03 from "./assets/images/cards/card_03.png";
+import card04 from "./assets/images/cards/card_04.png";
+import card05 from "./assets/images/cards/card_05.png";
+import card06 from "./assets/images/cards/card_06.png";
+import card07 from "./assets/images/cards/card_07.png";
+import card08 from "./assets/images/cards/card_08.png";
+import card09 from "./assets/images/cards/card_09.png";
+import card10 from "./assets/images/cards/card_10.png";
+import card11 from "./assets/images/cards/card_11.png";
+import card12 from "./assets/images/cards/card_12.png";
+import card13 from "./assets/images/cards/card_13.png";
+import card14 from "./assets/images/cards/card_14.png";
+import card15 from "./assets/images/cards/card_15.png";
+import card16 from "./assets/images/cards/card_16.png";
+import card17 from "./assets/images/cards/card_17.png";
+import card18 from "./assets/images/cards/card_18.png";
+import card19 from "./assets/images/cards/card_19.png";
+import card20 from "./assets/images/cards/card_20.png";
+import card21 from "./assets/images/cards/card_21.png";
+import card22 from "./assets/images/cards/card_22.png";
+import card23 from "./assets/images/cards/card_23.png";
 
 const CARD_IMAGE_MODULES = [
   card01,
